173ceab557b7e41d5eb7e4fb2db20368ad73abf2
Andres Romero <romeroandre801@outlook.com>
2023-05-15 22:00:43 -0600

    Update translator.py
    Added additional context comments to help code readability.

--- translator.py
+++ translator.py
@@ -1,11 +1,11 @@
 from translate import Translator
 
-translator = Translator(to_lang="ja")
+translator = Translator(to_lang="ja") #converts text to Japanese, please reference: https://en.wikipedia.org/wiki/ISO_639-1
 try:
-    with open('../test.txt', mode='r') as my_file:
+    with open('../input-file.txt', mode='r') as my_file:
         text = my_file.read()
         translation = translator.translate(text)
-        with open('../test-ja.txt', mode='w', encoding='utf-8') as my_file2:
+        with open('../output-file.txt', mode='w', encoding='utf-8') as my_file2:
             my_file2.write(translation)
 except FileNotFoundError as e:
     print('Check your filepath!')
